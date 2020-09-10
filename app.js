ea29b53f3ad8f648314f9820ee7d37fe991944bb
Omar Zain <omaren2010@gmail.com>
2020-09-10 14:04:26 +0300

    problem 1 Solved

--- app.js
+++ app.js
@@ -7,11 +7,20 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sum(a, b) { //eslint-disable-line
+var sum = a + b;
+var message1 = 'The sum of ' + a +' and '+ b +' is 11.';
+var arr1 = [sum , message1];
+alert(message1);
+console.log();
+return arr1;
+
 
 }
 
+
+
 // Here is the test for sum(); uncomment it to run it
-// testSum(4, 7);
+testSum(4, 7);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
