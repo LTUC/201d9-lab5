1544f59c66f5e0b2188cd0cf71f53faba14d7855
malekshmes <malekshmes@gmail.com>
2020-09-10 23:51:29 +0300

    problem 4 solved

--- app.js
+++ app.js
@@ -56,7 +56,7 @@ function sumAndMultiply(a, b, c) { //eslint-disable-line
     var total = sum(c, summatoinOfTheFirstTwo)[0]; // to add c to the sum
     var multiplicationOfTheFirstTwo = multiply(a, b)[0]; //to multiply a by b
     var total2 = multiply(c, multiplicationOfTheFirstTwo)[0]; //to multiply the result by c
-    var firstSentence = a + ' and ' + b + ' and ' + c + ' sum to ' + total + '.'; 
+    var firstSentence = a + ' and ' + b + ' and ' + c + ' sum to ' + total + '.';
     //console.log(firstSentence);
     var secondSentence = 'The product of ' + a + ' and ' + b + ' and ' + c + ' is ' + total2 + '.';
     //console.log(secondSentence);
@@ -72,7 +72,9 @@ testSumAndMultiply(4, 7, 5);
 
 /////////////////////////////////////
 /* Problem 4
-Write a function called sumArray() that takes in an array of numbers as its single argument and then returns an array where the first element is the sum of the numbers in the array, and the second element is a string that EXACTLY follows this example and uses the values that were input into the function:
+Write a function called sumArray() that takes in an array of numbers as its single argument and then returns
+an array where the first element is the sum of the numbers in the array, and the second element is a string that EXACTLY follows this 
+example and uses the values that were input into the function:
 "2,3,4 was passed in as an array of numbers, and 9 is their sum."
 IMPORTANT DETAIL: You may not use the arithmetic operator + in this function. To do addition, use your sum() function that you've already created. You're going to have to be resourceful to figure out how to do this. However, you may continue to use the + operator for string concatenation.
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSumArray() function and see if the test passes.*/
@@ -81,12 +83,19 @@ Test this function by hand in the console to get it working, and when you think 
 var testArray = [2, 3, 4]; //eslint-disable-line
 
 function sumArray(sumArr) { //eslint-disable-line
-
-}
+        var sumOfnum = sum(2,3)[0];
+        var sum3 = sum(4, sumOfnum)[0];        
+        //var secondElement = testArray[0, 1, 2];
+        var statement =testArray+ ' was passed in as an array of numbers, and ' + sum3 + ' is their sum.';
+        //console.log(testArray);
+        var arr = [sum3, statement];
+        //console.log(sumOfnum, testArray, statement);
+        return arr;
+    }
 
 // Here is the test for sumArray(); uncomment it to run it
 
-// testSumArray(testArray);
+testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
