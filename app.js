70279dc1e6b76be905fc6b7da7fa0df970f33869
Maisabdalrazeq <maisabdalrazq@gmail.com>
2020-09-10 17:38:53 +0300

    I fixed propblem5

--- app.js
+++ app.js
@@ -95,12 +95,15 @@ IMPORTANT DETAIL: You may not use the arithmetic operator * in this function. To
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiplyArray() function and see if the test passes.*/
 
 // Write your code here
-function multiplyArray(multArr) { //eslint-disable-line
-
+function multiplyArray([a , b , d]) { //eslint-disable-line
+var testArray = [2, 3, 4]; //eslint-disable-line
+var multiplyResult = multiply(a,b)[0];
+var totalResult = multiply(multiplyResult,d)[0];
+return [totalResult ,"The numbers " + a + "," + b + "," + d + " have a product of " + totalResult + "." ];
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyArray(testArray);
+testMultiplyArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
 
