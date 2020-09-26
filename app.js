ce0636e56562ea98f34e91c857abca7717b815db
rand alsallaq <randalsallaq97@gmail.com>
2020-09-26 11:23:04 +0300

    issue 5 is solved

--- app.js
+++ app.js
@@ -107,10 +107,14 @@ Test this function by hand in the console to get it working, and when you think 
 // Write your code here
 function multiplyArray(multArr) { //eslint-disable-line
 
+    var multiIssue5=multiply(testArray[0]*testArray[1], testArray[2]);
+    var msg5= 'The numbers '+multArr[0]+','+multArr[1]+','+multArr[2]+' have a product of '+multiIssue5[0]+'.';
+    var arr5=[multiIssue5[0],msg5];
+    return arr5;
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyArray(testArray);
+testMultiplyArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
 
